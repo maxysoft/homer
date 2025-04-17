7bfcbc0cb252ef531b3a70db595a7b1ae3cc64df
Tomas <1979739+maxysoft@users.noreply.github.com>
2025-04-17 19:36:14 +0000

    Forgot plugin

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,8 @@ import { fileURLToPath, URL } from "url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
+import { createHtmlPlugin } from "vite-plugin-html"; 
+
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "",
